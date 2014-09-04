79cd0738dba3ea6cdfd1d91acad03864ba47e192
Chris Keil <ckeil0689@gmail.com>
2014-09-04 16:53:42 +0200

    added buttons to switch styles (switching not implemented)

--- Etch-a-sketch/js/etch.js
+++ Etch-a-sketch/js/etch.js
@@ -4,36 +4,54 @@ $(document).ready(function() {
 
 		$('#initial').remove();
 		$('#header').show();
-		$('#canvas').show();
-		$('#resetBtn').show();
+		$('#selectStyle').show();
 
-		var cw = $('#container').width() - 20;
-		var ch = $('#container').height() - 100;
-
-
-		//Assume 15px width + height per element (10 size, 2 margin)
-		var xNum = Math.floor(cw/12);
-		var yNum = Math.floor(ch/12);
-
-		var divNum = xNum * yNum;
-
-		for(var i = 0; i < divNum; i++) {
-
-			$('#canvas').append("<div class='untouched'></div>");
-		}
-
-		$('div .untouched').hover(function() {
-			
-			var hex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-			$(this).css('background', hex);
-
-		}, function() {
-
-		});
+		setCanvas();
 	});
 
 	$('#resetBtn').click(function() { 
-
-		$('div .untouched').css('background', '#fff');
+		
+		$('#canvas').empty();
+		setCanvas();
 	});
 });
+
+var setCanvas = function() {
+
+	var cw = $('#container').width() - 20;
+	var ch = $('#container').height() - 100;
+
+	//Assume 10px width + height per element (8 size, 2 margin)
+	var dim = 12;
+	var xNum = Math.floor(cw/dim);
+	var yNum = Math.floor(ch/dim);
+
+	var divNum = xNum * yNum;
+
+	var treasureNum = 0;
+	var $canvas = $('#canvas');
+	for(var i = 0; i < divNum; i++) {
+
+		$canvas.append("<div class='paint'></div>");
+	}
+
+	$('#canvas').show();
+	$('#resetBtn').show();
+
+	$('div .paint').hover(function() {
+
+		var hex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+		$(this).css('background', hex);
+
+	}, function() {
+
+	});
+
+	// $('div .treasure').hover(function() {
+
+	// 	// window.location='www.reddit.com';
+
+	// }, function() {
+
+	// });
+};
